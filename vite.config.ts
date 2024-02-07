fdd5baf1eb91b402b4b55496a7daa2ecd3cdd2c0
aryanxarora <aryanxarora@gmail.com>
2024-02-07 13:16:27 -0500

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,56 @@
+import { defineConfig } from "vitest/config"
+import react from "@vitejs/plugin-react"
+import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
+
+const manifestPWA: Partial<VitePWAOptions> = {
+  registerType: "prompt",
+  includeAssets: ["vite.svg"],
+  manifest: {
+    "id": "vite-pwa-app",
+    "scope": "/",
+    "name": "vite-pwa-app",
+    "short_name": "vite-pwa-app",
+    "display": "standalone",
+    "start_url": "/",
+    "theme_color": "#000000",
+    "background_color": "#000000",    
+    "description": "desc",
+    "icons": [
+        {
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
+    ],
+    "categories": ["education", "productivity"],
+  }
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), VitePWA(manifestPWA)],
+  server: {
+    open: true,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "src/setupTests",
+    mockReset: true,
+  },
+})
