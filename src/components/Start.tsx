b393de13c2ce2582488bcf40f91a6ff52772e9bd
aryanxarora <aryanxarora@gmail.com>
2024-02-07 15:17:23 -0500

    TS + Tailwind config

--- src/components/Start.tsx
+++ src/components/Start.tsx
@@ -1,8 +1,13 @@
 const Start = () => {
   return (
-    <div className="w-full h-screen flex flex-col text-center justify-center items-center text-sky-500">
-      <h1 className="font-bold text-3xl">Vite.js PWA</h1>
-      <ul className="mt-10">
+    <div className="w-full h-screen flex flex-col text-center justify-center items-center text-sky-500 gap-10">
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/2300px-React-icon.svg.png"
+        alt="react"
+        className="w-20 animate-spin"
+      />
+      <h1 className="text-3xl font-serif">Vite.js PWA</h1>
+      <ul className="font-mono">
         <li>React (Vite)</li>
         <li>Progressive Web App (PWA)</li>
         <li>TypeScript</li>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,29 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
+    fontFamily: {
+      'sans' : ['Inter', 'ui-sans-serif', 'system-ui'],
+      'serif': ['Playfair Display'],
+      'mono' : ['IBM Plex Mono']
+    },
+    fontSize: {
+      xs: '0.65rem',
+      sm: '0.75rem',
+      md: '0.88rem',
+      base: '1rem',
+      xl: '1.25rem',
+      '2xl': '1.563rem',
+      '3xl': '1.953rem',
+      '4xl': '2.441rem',
+      '5xl': '3.052rem',
+    },
   },
   plugins: [],
 }
